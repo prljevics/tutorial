57c1ab39cdc8c9057d447bc95c62864a5cd1d504
Slobodan Prljevic <prljevics@gmail.com>
2022-02-16 21:13:21 +0100

    Properties

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import "./index.css";
+
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+  title: "I Love You To The Moon an Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/51vYL46df+L.jpg",
+  title: "The Eye of the World: Book One of The Wheel of Time",
+  author: "Robert Jordan",
+};
+
 function Booklist() {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book book={firstBook} />
+      <Book book={secondBook} />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
-    <article>
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+    <article className="book">
+      <img className="image" src={props.book.img} alt="" />
+      <h1 className="title">{props.book.title}</h1>
+      <h4 className="author">{props.book.author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
-    alt=""
-  />
-);
-
-const Author = () => <h4>Amelia Hepworth</h4>;
-
-const Title = () => <h1>I Love You To The Moon an Back</h1>;
 
 ReactDom.render(<Booklist />, document.getElementById("root"));

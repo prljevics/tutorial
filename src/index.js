7687a15ef674f9777134894e79054d575cd296f1
Slobodan Prljevic <prljevics@gmail.com>
2022-02-19 20:49:13 +0100

    Event handlers

--- src/index.js
+++ src/index.js
@@ -34,13 +34,29 @@ function Booklist() {
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    alert("Hello world");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img className="image" src={img} alt="" />
       <h1 className="title">{title}</h1>
       <h4 className="author">{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Buy
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More complex example
+      </button>
     </article>
   );
 };

819e301c89a5c1eff976a88a79c80b42a8926fdd
Slobodan Prljevic <prljevics@gmail.com>
2022-02-19 16:54:40 +0100

    Render list of objects

--- src/index.js
+++ src/index.js
@@ -2,35 +2,45 @@ import React from "react";
 import ReactDom from "react-dom";
 
 import "./index.css";
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+    title: "I Love You To The Moon an Back",
+    author: "Amelia Hepworth",
+  },
 
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
-  title: "I Love You To The Moon an Back",
-  author: "Amelia Hepworth",
-};
-
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/51vYL46df+L.jpg",
-  title: "The Eye of the World: Book One of The Wheel of Time",
-  author: "Robert Jordan",
-};
-
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/51vYL46df+L.jpg",
+    title: "The Eye of the World: Book One of The Wheel of Time",
+    author: "Robert Jordan",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/91HSa7sG8tL._AC_UY218_.jpg",
+    title:
+      "A Game of Thrones / A Clash of Kings / A Storm of Swords / A Feast of Crows / A Dance with Dragons",
+    author: "J.R.R. Martin",
+  },
+];
 function Booklist() {
   return (
     <section className="booklist">
-      <Book book={firstBook} />
-      <Book book={secondBook} />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img className="image" src={props.book.img} alt="" />
-      <h1 className="title">{props.book.title}</h1>
-      <h4 className="author">{props.book.author}</h4>
+      <img className="image" src={img} alt="" />
+      <h1 className="title">{title}</h1>
+      <h4 className="author">{author}</h4>
     </article>
   );
 };
